e5e98f97df641f185d7d992956dbcebe3d837ec7
Liana <liana.merk@gmail.com>
2022-04-04 00:04:06 +0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+long_description = "hovercal uses holoviews to create customizable plots from pandas timeseries data. Panel is used to create a global heatmap and arrange the plots. The user input is a dataframe containing dates and values. The package includes a cleaning function for plotting directly from spotify json extended listening history."
+
+setup(
+    name="hovercal",
+    version="0.0.1",
+    description="hovercal is a package for visualizing data on a calendar heatmap",
+    url="https://github.com/lianamerk/hovercal",
+    long_description=long_description,
+    license="MIT",
+    author='Liana Merk',
+    author_email='liana.merk@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy','scipy','pandas', 'bokeh','holoviews','panel', 'glob'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
